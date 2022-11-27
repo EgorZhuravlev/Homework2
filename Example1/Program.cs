4b737ad994afcabb8cf5b3c3d714b771bfa5cda8
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-11-27 18:00:47 +0300

    Example1 added

---
+++ Example1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isParsed)
+{
+    Console.WriteLine("Введено неправильное значение");
+}
+
+int secondNumber = (number % 100 - number % 10) / 10;
+
+if (number > 100 & number < 1000)
+{
+    Console.WriteLine(secondNumber);
+}
+
