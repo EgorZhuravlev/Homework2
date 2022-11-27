e1c29c8f07d0ff2b7f28ef89f2a30fc0479aa181
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-11-27 19:58:34 +0300

    Example2 added

---
+++ Example2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsed)
+{
+    Console.WriteLine("Введено неправильное значение");
+}
+
+if (n >= 100)
+{
+    while (n > 999)
+    {
+        n /= 10;
+    }
+    int result = n % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
